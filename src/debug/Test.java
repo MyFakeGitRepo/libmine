96c31927232cb85d031bf6ffd57d0ea6875673da
canard <canard@localhost>
2018-03-20 23:18:04 -0400

    Creation of the first package

---
+++ src/debug/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package debug;
+
+/**
+ * @author canard
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It works !");
+	}
+
+}
